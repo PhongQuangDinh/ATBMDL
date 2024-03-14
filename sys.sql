b79df91ebb7567579a1f28d1cacb9d4dfc7de574
Võ Cao Trí <trivo.131003@gmail.com>
2024-03-14 13:14:25 +0700

    update error

--- sys.sql
+++ sys.sql
@@ -1,7 +1,20 @@
-create user c##ad IDENTIFIED by 123;
-GRANT CREATE SESSION TO c##ad;
-CONNECT c##ad/123; 
+alter session set current_schema = sys;
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+create user ad IDENTIFIED by 123;
+GRANT CREATE SESSION TO ad container = all;
+CONNECT ad/123; 
 
-Grant DBA TO c##ad container = all;
-GRANT ALL PRIVILEGES TO c##ad;
+Grant DBA TO ad container = all;
+
+--GRANT ALL PRIVILEGES TO ad;
+
 SHOW CON_NAME;
+
+SELECT SYS_CONTEXT('USERENV', 'CON_NAME') from dual;
+
+SELECT * FROM ALL_USERS ORDER BY USERNAME;
+
+
+select value
+from nls_database_parameters
+where parameter = 'NLS_CHARACTERSET';
